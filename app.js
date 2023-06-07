8d572b2cfea6523869444e358de15f2bc11e020b
Kassandra Gonzalez <134988924+jkasss@users.noreply.github.com>
2023-06-06 21:21:22 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const taskInput = document.getElementById(task-input);
+const addTaskBtn = document.getElementById(add-task-btn);
+const taskList = document.getElementById(task-list);
+
+//Función para agregar una nueva tarea
+function addTask() {
+ const taskTex = taskInput.value;
+ if (taskTex.trim() !==
+
+
+
+}
